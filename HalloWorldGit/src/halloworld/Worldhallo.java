0784183767f442683bd968a0ade4a45eb380473e
moussa <mamoussa@zonnet.nl>
2018-10-19 23:04:32 +0200

    first commit

---
+++ HalloWorldGit/src/halloworld/Worldhallo.java
@@ -0,0 +1,12 @@
+package halloworld;
+
+public class Worldhallo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hallo World!!");
+		
+
+	}
+
+}
